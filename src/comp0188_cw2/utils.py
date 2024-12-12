e491ee76dd3906f148e5868631e7fb7a89561089
Dion Fernandes <dionfernandes5@gmail.com>
2024-12-12 17:25:30 +0530

    Fixed type annotations in to_greyscale to use height_width naming

--- src/comp0188_cw2/utils.py
+++ src/comp0188_cw2/utils.py
@@ -16,8 +16,8 @@ from .config import WANDB_PROJECT
 @jaxtyped(typechecker=typechecker)
 
 def to_greyscale(
-    c_img: Int32[Array, "height width channels"],  # Input: Integer NumPy array
-) -> Float32[Array, "height width"]:      # Output: Float NumPy array
+    c_img: Int32[Array, "height_width channels"],  # Input: Integer NumPy array
+) -> Float32[Array, "height_width"]:      # Output: Float NumPy array
 
     """Converts numpy array of dimension: 
     (batch_size, height, width, filters), defining an RGB image to a greyscale 
