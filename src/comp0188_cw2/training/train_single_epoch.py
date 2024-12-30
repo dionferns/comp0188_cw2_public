34b4260ae4e42650a236b18a4b532b325e8e9aa5
Dion Fernandes <dionfernandes5@gmail.com>
2024-12-30 12:32:55 +0530

    alterations to utils.py

--- src/comp0188_cw2/training/train_single_epoch.py
+++ src/comp0188_cw2/training/train_single_epoch.py
@@ -119,6 +119,10 @@ class TrainSingleEpoch:
             try:
                 # compute gradient and do SGD step
                 train_loss.backward()
+                
+
+                #change made here.
+                torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
 
                 optimizer.step()
             except RuntimeError as e:
